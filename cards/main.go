e77ab7966d152facb31d79c02486e08fbd4e5fa4
Colin Eckert <colineckert10@gmail.com>
2024-08-05 12:14:03 -0700

    create deck class

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "King of Hearts")
 
-	fmt.Println(card)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
+func newCard() string {
+	return "Five of Diamonds"
+}
