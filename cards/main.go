7eaff5670af5183ea7263d8fc29702708f768889
Colin Eckert <colineckert10@gmail.com>
2024-08-05 13:39:48 -0700

    write deck print receiver

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "King of Hearts")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
