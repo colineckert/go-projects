e985569635bd2b62bf62259a561d4b6fcb84e481
Colin Eckert <colineckert10@gmail.com>
2024-08-06 15:08:04 -0700

    write newDeckFromFile

--- cards/deck.go
+++ cards/deck.go
@@ -39,6 +39,17 @@ func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
