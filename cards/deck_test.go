f5b4a8649b4cc8b785ad499015bb9daddba111a9
Colin Eckert <colineckert10@gmail.com>
2024-08-07 17:25:32 -0700

    finish tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -20,3 +21,16 @@ func TestNewDeck(t *testing.T) {
 	}
 }
 
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	if (len(loadedDeck)) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
