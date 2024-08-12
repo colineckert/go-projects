86596fab7f991d70a9d1cc3ab8d3d3a2e7aef2f0
Colin Eckert <colineckert10@gmail.com>
2024-08-12 08:03:41 -0700

    add second option

--- print-file/main.go
+++ print-file/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
+// func main() {
+// 	content, err := os.ReadFile(os.Args[1])
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		os.Exit(1)
+// 	}
+
+// 	os.Stdout.Write(content)
+// }
+
 func main() {
-	content, err := os.ReadFile(os.Args[1])
+	filePtr, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-
-	os.Stdout.Write(content)
+	io.Copy(os.Stdout, filePtr)
 }
