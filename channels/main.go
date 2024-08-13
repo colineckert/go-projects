27794abe153e421b7e2386a706a2786798c4a281
Colin Eckert <colineckert10@gmail.com>
2024-08-13 07:22:22 -0700

    use channels to receive messages

---
+++ channels/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.amazon.com",
+		"https://www.stackoverflow.com",
+		"https://www.golang.org",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- "Might be down I think!"
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- "Yep it's up!"
+}
