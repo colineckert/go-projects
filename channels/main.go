2d21a05f94a838f89b400f67acb91648eaa96fc1
Colin Eckert <colineckert10@gmail.com>
2024-08-13 10:04:44 -0700

    infinitely poll endpoints

--- channels/main.go
+++ channels/main.go
@@ -20,8 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -29,10 +29,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think!"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep it's up!"
+	c <- link
 }
